90cc0b1e29eecd6d862800f5dc2ac149fcdee00e
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-16 12:25:44 -0300

    feat: se encapsulan la lista de libros recibida para su manejo en un LiveData

--- app/src/main/java/com/anchorbooks/bestseller/model/repository/Repository.kt
+++ app/src/main/java/com/anchorbooks/bestseller/model/repository/Repository.kt
@@ -1,16 +1,19 @@
 package com.anchorbooks.bestseller.model.repository
 
+import androidx.lifecycle.MutableLiveData
 import com.anchorbooks.bestseller.model.remote.RetrofitClient
+import com.anchorbooks.bestseller.model.remote.pojo.Book
 import timber.log.Timber
 
 class Repository {
+    val books : MutableLiveData<List<Book>> = MutableLiveData()
     suspend fun listBooks() {
         Timber.d("listBooks")
         val response = RetrofitClient.retrofitInstance().booksList()
 
         when {
             response.isSuccessful -> response.body()?.let {
-                Timber.d("tenemos ${it.size} libros")
+                books.value = it
             }
             else -> Timber.d("${response.errorBody()}")
         }
--- app/src/main/java/com/anchorbooks/bestseller/ui/vm/VMBookListing.kt
+++ app/src/main/java/com/anchorbooks/bestseller/ui/vm/VMBookListing.kt
@@ -1,7 +1,9 @@
 package com.anchorbooks.bestseller.ui.vm
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.anchorbooks.bestseller.model.remote.pojo.Book
 import com.anchorbooks.bestseller.model.repository.Repository
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -9,10 +11,15 @@ import timber.log.Timber
 class VMBookListing : ViewModel() {
     private val repository = Repository()
 
+    private val books = repository.books
+
     init {
         Timber.d("Cargando el listado de libros")
         viewModelScope.launch {
             repository.listBooks()
         }
     }
+
+    fun books(): LiveData<List<Book>> = books
+
 }
