4c62adbc504faa528c0968de2938868364816b42
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-16 10:33:49 -0300

    feat(MainActivity): inicializando timber para un mejor manejo de logs

--- app/src/main/java/com/anchorbooks/bestseller/MainActivity.kt
+++ app/src/main/java/com/anchorbooks/bestseller/MainActivity.kt
@@ -2,10 +2,13 @@ package com.anchorbooks.bestseller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anchorbooks.bestseller.utils.TimberInitializer
 
 class MainActivity : AppCompatActivity() {
+    private val timberInitializer = TimberInitializer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timberInitializer.initTimber()
     }
 }
