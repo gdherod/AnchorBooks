6c6bf486d5f9383d2bcde1cebba3402bdc690574
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-16 14:59:25 -0300

    feat(ApiConsumer): nueva función suspendida para traer el detalle de un libro

--- app/src/main/java/com/anchorbooks/bestseller/model/remote/ApiConsumer.kt
+++ app/src/main/java/com/anchorbooks/bestseller/model/remote/ApiConsumer.kt
@@ -1,14 +1,19 @@
 package com.anchorbooks.bestseller.model.remote
 
 import com.anchorbooks.bestseller.model.remote.pojo.Book
+import com.anchorbooks.bestseller.model.remote.pojo.BookDetail
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BookAPI {
     @GET("books/")
     suspend fun booksList(): Response<List<Book>>
+
+    @GET("bookDetail/{id}")
+    suspend fun bookDetail(@Path("id") id: Int): Response<BookDetail>
 }
 
 class RetrofitClient {
