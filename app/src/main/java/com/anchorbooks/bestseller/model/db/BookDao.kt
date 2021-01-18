50f45fcc38ca62266a34304e7e31c40b97db69f6
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-18 17:24:34 -0300

    refactor(BookDao): añadido que hacer en caso de conflictos al insertar datos en las tablas de la bd. mejoras en los nombres de las funciones.

--- app/src/main/java/com/anchorbooks/bestseller/model/db/BookDao.kt
+++ app/src/main/java/com/anchorbooks/bestseller/model/db/BookDao.kt
@@ -3,20 +3,21 @@ package com.anchorbooks.bestseller.model.db
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface BookDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(books: List<BookEntity>)
 
     @Query("SELECT * FROM book_table")
     fun getBooks(): LiveData<List<BookEntity>>
 
-    @Insert
-    suspend fun insert(book: BookDetailEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(bookDetail: BookDetailEntity)
 
     @Query("SELECT * FROM book_detail_table WHERE id= :id")
-    fun getBook(id: Int): LiveData<BookDetailEntity>
+    fun getBookDetail(id: Int): LiveData<BookDetailEntity>
 }
