aacb13143382384b4dabb1298354dba4ecb703c7
Gary Hermosilla <garyhermosilla@gmail.com>
2021-01-20 22:30:51 -0300

    refactor(BookMappers): reformateo de código, además se agrega funcion para agregar los detalles de un libro a la base de datos para tener persistencia local de dichos datos

--- app/src/main/java/com/anchorbooks/bestseller/model/db/BookMappers.kt
+++ app/src/main/java/com/anchorbooks/bestseller/model/db/BookMappers.kt
@@ -1,11 +1,43 @@
 package com.anchorbooks.bestseller.model.db
 
 import com.anchorbooks.bestseller.model.remote.pojo.Book
+import com.anchorbooks.bestseller.model.remote.pojo.BookDetail
 
 fun mapperBookApiToDB(book: Book): BookEntity {
-    return BookEntity(book.id, book.author, book.country, book.imageLink, book.language, book.title)
+    return BookEntity(
+        book.id,
+        book.author,
+        book.country,
+        book.imageLink,
+        book.language,
+        book.title
+    )
 }
 
 fun mapperBookDBToApi(bookEntity: BookEntity): Book {
-    return Book(bookEntity.id, bookEntity.author, bookEntity.country, bookEntity.imageLink, bookEntity.language, bookEntity.title)
+    return Book(
+        bookEntity.id,
+        bookEntity.author,
+        bookEntity.country,
+        bookEntity.imageLink,
+        bookEntity.language,
+        bookEntity.title
+    )
+}
+
+fun mapperBookDetailApiToDB(bookDetail: BookDetail): BookDetailEntity {
+    return BookDetailEntity(
+        bookDetail.id,
+        bookDetail.author,
+        bookDetail.country,
+        bookDetail.imageLink,
+        bookDetail.language,
+        bookDetail.link,
+        bookDetail.pages,
+        bookDetail.title,
+        bookDetail.year,
+        bookDetail.price,
+        bookDetail.lastPrice,
+        bookDetail.delivery
+    )
 }
